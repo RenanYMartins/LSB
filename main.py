76672265b48cae80df4d624037f0310b07875d17
RenanYMartins <yohanssen.martins@gmail.com>
2025-09-18 22:06:23 -0300

    :sparkles: conversoes para ascii e binario

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 def main():
-    texto = str("A");
-
     # Receber um texto
+    text = str("Tabela ASCII eh bem legal");
+
     # Converter em ASCII (decimal)
+    decoded = _text_to_ascii(text)
+
     # Converter em binário
+    binary = _ascii_to_binary(decoded)
+
+    print(binary)
+
+def _text_to_ascii(text):
+    decode_list = []
+
+    for letter in text:
+        decoded = int(ord(letter))
+        decode_list.append(decoded)
+
+    return decode_list
+
+def _ascii_to_binary(decoded):
+    binary_list = []
+
+    for ascii in decoded:
+        binary = format(ascii, "08b")
+        binary_list.append(binary)
+
     # Formar um array de bits
+    return ' '.join(binary_list)
+        
+
+
 
     # Receber o path da imagem
     # Converter imagem para bits
