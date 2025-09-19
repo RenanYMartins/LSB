b91ff09504a9752bc83e5f34eebb218a9b7c3722
RenanYMartins <yohanssen.martins@gmail.com>
2025-09-18 21:20:34 -0300

    :sparkles: first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    texto = str("A");
+
+    # Receber um texto
+    # Converter em ASCII (decimal)
+    # Converter em binário
+    # Formar um array de bits
+
+    # Receber o path da imagem
+    # Converter imagem para bits
+
+    # inserir bits do texto nos bits da imagem
+    # gerar a imagem e salvar em um diretorio
+
+
+
+if __name__ == "__main__":
+    main()
